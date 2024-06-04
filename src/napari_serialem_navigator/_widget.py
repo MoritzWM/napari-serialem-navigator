0a8953d438c890b3246c486a408fc80360386d49
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-06-04 09:54:04 +0200

    Satisfy pyright

--- src/napari_serialem_navigator/_widget.py
+++ src/napari_serialem_navigator/_widget.py
@@ -5,27 +5,28 @@ from magicgui.widgets import ComboBox, PushButton, Container, create_widget
 from skimage.transform import estimate_transform
 
 if TYPE_CHECKING:
-    import napari
+    from napari.viewer import Viewer
+    from napari.layers import Shapes, Image
 
 
 class LinkMap(Container):
-    def __init__(self, viewer: 'napari.viewer.Viewer'):
+    def __init__(self, viewer: 'Viewer'):
         super().__init__()
         self.viewer = viewer
 
 
         self.wdg_shapes_layer = create_widget(
             label="Navigator shapes",
-            annotation="napari.layers.Shapes"
+            annotation="Shapes"
         )
         self.wdg_image_layer = create_widget(
             label="Map image",
-            annotation="napari.layers.Image"
+            annotation="Image"
         )
         self.wdg_polygon = ComboBox(label="Polygon", choices=self.polygon_names)
         self.wdg_link = PushButton(text="Link")
         self.wdg_link.clicked.connect(self.link)
-        self.wdg_shapes_layer.changed.connect(self.wdg_polygon.reset_choices)
+        self.wdg_shapes_layer.changed.connect(self.wdg_polygon.reset_choices)  # type: ignore
         self.extend([
             self.wdg_shapes_layer,
             self.wdg_image_layer,
@@ -34,12 +35,12 @@ class LinkMap(Container):
         ])
         
     @property
-    def shapes_layer(self) -> Optional["napari.layers.Shapes"]:
+    def shapes_layer(self) -> Optional["Shapes"]:
         assert isinstance(self.wdg_shapes_layer, ComboBox)
         return self.wdg_shapes_layer.value
 
     @property
-    def image_layer(self) -> Optional["napari.layers.Image"]:
+    def image_layer(self) -> Optional["Image"]:
         assert isinstance(self.wdg_image_layer, ComboBox)
         return self.wdg_image_layer.value
 
