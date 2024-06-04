746a2e8cf69e55b22d58e14f29cc344b25e1b68c
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-06-04 10:01:01 +0200

    Formatting

--- src/napari_serialem_navigator/_reader.py
+++ src/napari_serialem_navigator/_reader.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import numpy as np
 from .serialem_navigator import SEMNavigator
 
+
 def napari_get_reader(path):
     if not isinstance(path, list):
         path = [path]
-    if not all(p.endswith('.nav') for p in path):
+    if not all(p.endswith(".nav") for p in path):
         return None
     return reader_function
 
@@ -23,13 +24,15 @@ def reader_function(path):
     point_coords = list()
     point_names = list()
     for item in nav.items:
-        ptsx_str, ptsy_str = item.get('PtsX'), item.get('PtsY')
+        ptsx_str, ptsy_str = item.get("PtsX"), item.get("PtsY")
         if ptsx_str is None or ptsy_str is None:
             continue
         ptsx = [float(x) for x in ptsx_str.split()]
         ptsy = [float(y) for y in ptsy_str.split()]
         if len(ptsx) != len(ptsy):
-            raise AttributeError(f'PtsX and PtsY have unequal length in item {item.id}')
+            raise ValueError(
+                f"PtsX and PtsY have unequal length in item {item.id}"
+            )
         if len(ptsx) == 1:
             point_coords.append((ptsy[0], ptsx[0]))
             point_names.append(str(item))
@@ -39,17 +42,31 @@ def reader_function(path):
             polygon_coords.append(np.array(list(zip(ptsy, ptsx))))
             polygon_names.append(str(item))
     point_coords = np.array(point_coords)
-    return [(polygon_coords, dict(name=f'{path.stem} polygons',
-                            shape_type='polygon',
-                            face_color='transparent',
-                            edge_color='red',
-                            edge_width=0.1,
-                            text=dict(
-                                string='{polygon_name}',
-                                anchor='upper_right',
-                                color="red",
-                            ),
-                            properties=dict(polygon_name=polygon_names)), 'shapes'),
-            (point_coords, dict(name=f'{path.stem} points',
-                          text='{point_name}',
-                          properties=dict(point_name=point_names)), 'points')]
+    return [
+        (
+            polygon_coords,
+            dict(
+                name=f"{path.stem} polygons",
+                shape_type="polygon",
+                face_color="transparent",
+                edge_color="red",
+                edge_width=0.1,
+                text=dict(
+                    string="{polygon_name}",
+                    anchor="upper_right",
+                    color="red",
+                ),
+                properties=dict(polygon_name=polygon_names),
+            ),
+            "shapes",
+        ),
+        (
+            point_coords,
+            dict(
+                name=f"{path.stem} points",
+                text="{point_name}",
+                properties=dict(point_name=point_names),
+            ),
+            "points",
+        ),
+    ]
